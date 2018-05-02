1d7976c3cc9e1ff187372d44305b0f0a1f92babb
taowu750 <2691320794@qq.com>
2018-05-02 09:41:03 +0800

    Add annotations for ImageView's BindingAdapter.

--- mvvm/src/main/java/com/wutaodsg/mvvm/bindingadapter/ImageViewBindingAdapter.java
+++ mvvm/src/main/java/com/wutaodsg/mvvm/bindingadapter/ImageViewBindingAdapter.java
@@ -5,11 +5,17 @@ import android.support.annotation.DrawableRes;
 import android.widget.ImageView;
 
 /**
- * Created by wutao on 2018/5/2.
+ * {@link ImageView} 的 BindingAdapter。
  */
 
 public class ImageViewBindingAdapter {
 
+    /**
+     * 设置 ImageView src 属性值为一个 Drawable 的资源 id。
+     *
+     * @param imageView {@link ImageView} 对象
+     * @param imageId   Image 的资源 id，属于 R.drawable 下
+     */
     @BindingAdapter("android:src")
     public static void setImageId(ImageView imageView, @DrawableRes int imageId) {
         imageView.setImageResource(imageId);
